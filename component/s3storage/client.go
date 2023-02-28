b0b2d541e8b44f375b0edfc24f1c312ed61aa651
David Habinsky <david.habinsky@seagate.com>
2023-02-28 13:28:36 -0700

    called wrapper in ReadBuffer() and ReadInBuffer()

--- component/s3storage/client.go
+++ component/s3storage/client.go
@@ -586,9 +586,7 @@ func (cl *Client) ReadToFile(name string, offset int64, count int64, fi *os.File
 
 	bucketName := cl.Config.authConfig.BucketName
 
-	var result *s3.GetObjectOutput
-
-	result, err = cl.getObject(name, offset, count)
+	result, err := cl.getObject(name, offset, count)
 
 	if err != nil {
 		// No such key found so object is not in S3
@@ -634,28 +632,7 @@ func (cl *Client) ReadBuffer(name string, offset int64, len int64) ([]byte, erro
 		buff = make([]byte, len)
 	}
 
-	var err error
-	var apiErr smithy.APIError
-	var result *s3.GetObjectOutput
-	for i := 0; i < retryCount; i++ {
-		result, err = cl.awsS3Client.GetObject(context.TODO(), &s3.GetObjectInput{
-			Bucket: aws.String(cl.Config.authConfig.BucketName),
-			Key:    aws.String(name),
-			Range:  aws.String("bytes=" + fmt.Sprint(offset) + "-" + fmt.Sprint(len)),
-		})
-		if err == nil {
-			break
-		}
-		// Lyve Cloud sometimes returns InvalidAccessKey even if the access key is correct and the request was correct
-		// So if we get this error we want to try again, and if not then we got a legitimate error so break
-		if errors.As(err, &apiErr) {
-			code := apiErr.ErrorCode()
-			if code != "InvalidAccessKeyId" {
-				break
-			}
-			log.Warn("Client::ReadBuffer Lyve Cloud \"Invalid Access Key\" bug - retry %d of %d.", i, retryCount)
-		}
-	}
+	result, err := cl.getObject(name, offset, len)
 
 	if err != nil {
 		// No such key found so object is not in S3
@@ -688,29 +665,7 @@ func (cl *Client) ReadBuffer(name string, offset int64, len int64) ([]byte, erro
 func (cl *Client) ReadInBuffer(name string, offset int64, len int64, data []byte) error {
 	log.Trace("Client::ReadInBuffer : name %s", name)
 
-	var err error
-	var apiErr smithy.APIError
-	var result *s3.GetObjectOutput
-	for i := 0; i < retryCount; i++ {
-		log.Trace("Offset is: ", offset, "  Length is: ", len)
-		result, err = cl.awsS3Client.GetObject(context.TODO(), &s3.GetObjectInput{
-			Bucket: aws.String(cl.Config.authConfig.BucketName),
-			Key:    aws.String(name),
-			Range:  aws.String("bytes=" + fmt.Sprint(offset) + "-" + fmt.Sprint(len+offset)),
-		})
-		if err == nil {
-			break
-		}
-		// Lyve Cloud sometimes returns InvalidAccessKey even if the access key is correct and the request was correct
-		// So if we get this error we want to try again, and if not then we got a legitimate error so break
-		if errors.As(err, &apiErr) {
-			code := apiErr.ErrorCode()
-			if code != "InvalidAccessKeyId" {
-				break
-			}
-			log.Warn("Client::ReadInBuffer Lyve Cloud \"Invalid Access Key\" bug - retry %d of %d.", i, retryCount)
-		}
-	}
+	result, err := cl.getObject(name, offset, len)
 
 	if err != nil {
 		// No such key found so object is not in S3
