39c2dd8e4d950628aff05ac3b696125a33dfec7d
James Matthew Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-03-07 10:14:51 -0700

    Update defaults for shell scripts

--- test-scripts/e2e-test.sh
+++ test-scripts/e2e-test.sh
@@ -20,7 +20,7 @@ echo "-------------------------------------------------------------------"
 echo "Starting End-to-End Test"
 
 # run test
-go test -v -timeout=2h ../test/e2e_tests/data_validation_test.go ../test/e2e_tests/dir_test.go ../test/e2e_tests/file_test.go -args -mnt-path=$MOUNT_DIR -adls=false -clone=true -tmp-path=$MOUNT_TMP -quick-test=true -stream-direct-test=false -distro-name="Ubuntu"
+go test -v -timeout=2h ../test/e2e_tests/data_validation_test.go ../test/e2e_tests/dir_test.go ../test/e2e_tests/file_test.go -args -mnt-path=$MOUNT_DIR -adls=false -clone=false -tmp-path=$MOUNT_TMP -quick-test=true -stream-direct-test=false -distro-name="Ubuntu"
 
 # Cleanup test
 source ./helper/cleanup.sh
--- test-scripts/stress-test.sh
+++ test-scripts/stress-test.sh
@@ -20,7 +20,7 @@ echo "-------------------------------------------------------------------"
 echo "Starting Stress Test"
 
 # run test
-go test -timeout=120m -v ../test/stress_test/stress_test.go -args -mnt-path=$MOUNT_DIR -quick=false
+go test -timeout=120m -v ../test/stress_test/stress_test.go -args -mnt-path=$MOUNT_DIR -quick=true
 
 # Cleanup test
 source ./helper/cleanup.sh
