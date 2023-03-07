3b52c1e6885fb6686a2ebc1bcd6b3b73dfdd4c55
LeataByers <Leata.Byers@Seagate.com>
2023-03-07 12:09:57 -0700

    Delete old UI & python files, inc compile

--- gui/compile_ui.sh
+++ gui/compile_ui.sh
@@ -7,9 +7,3 @@ pyside6-uic lyve_config_common.ui > ui_lyve_config_common.py
 pyside6-uic azure_config_common.ui > ui_azure_config_common.py
 pyside6-uic azure_config_advanced.ui > ui_azure_config_advanced.py
 pyside6-uic lyve_config_advanced.ui > ui_lyve_config_advanced.py
-#pyside6-uic AzModeOptions.ui > ui_AzModeOptions.py
-#pyside6-uic FCAz_simpleSettings.ui > ui_FCAz_simpleSettings.py
-#pyside6-uic StAz_simpleSettings.ui > ui_StAz_simpleSettings.py
-#pyside6-uic FCL_simpleSettings.ui > ui_FCL_simpleSettings.py
-#pyside6-uic StL_simpleSettings.ui > ui_StL_simpleSettings.py
-#pyside6-uic mountSettings.ui > ui_mountSettings.py
