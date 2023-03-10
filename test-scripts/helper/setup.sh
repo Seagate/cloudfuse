91b05828fa79a21c4b6935cdc40b11afc4860f6e
James Matthew Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-03-10 16:52:33 -0700

    Explain how to create environment variable file

--- test-scripts/helper/setup.sh
+++ test-scripts/helper/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# The following creates a file called var.env and then adds the lines
+# up until the EOF to the file. This is based on the following
+# stack overflow post 
+# https://stackoverflow.com/questions/4879025/creating-files-with-some-content-with-shell-script
+
 cat <<EOF >var.env
 # This is the mount directory that the container will mount in
 export MOUNT_DIR=~/e2e-test
