fd396ceb92e36344acb4e728abf43562f7a88271
David Habinsky <david.habinsky@seagate.com>
2023-03-14 13:29:56 -0600

    add package and install trivy in script

--- TrivySetup.sh
+++ TrivySetup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cat /etc/apt/sources.list
-sudo apt-get install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
