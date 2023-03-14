69e2c3eceafcba4fbb2ab38b8f92593a6243836e
James Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-03-14 16:15:01 -0600

    Use Winfsp version that does not require reboot

--- install_winfsp.bat
+++ install_winfsp.bat
@@ -1,6 +1,6 @@
 rem This batch file will download and install WinFSP
 rem Usee bitsadmin to download the MSI file from the WinFSP github
-bitsadmin /transfer winfsp /priority high https://github.com/winfsp/winfsp/releases/download/v1.12.22339/winfsp-1.12.22339.msi %temp%\winfsp
+bitsadmin /transfer winfsp /priority high https://github.com/winfsp/winfsp/releases/download/v2.0RC1/winfsp-2.0.23055.msi %temp%\winfsp
 
 rem Uses msiexec to install the MSI file silently
 msiexec /i %temp%\winfsp /quiet /norestart ADDLOCAL=Core,Developer
