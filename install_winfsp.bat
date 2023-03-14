f42ae1f03ac018bba0e354f40f49840152b12118
James Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-03-14 16:35:51 -0600

    Update batch file to install all options and install winfsp before setting up go

--- install_winfsp.bat
+++ install_winfsp.bat
@@ -2,8 +2,8 @@ rem This batch file will download and install WinFSP
 rem Usee bitsadmin to download the MSI file from the WinFSP github
 bitsadmin /transfer winfsp /priority high https://github.com/winfsp/winfsp/releases/download/v2.0RC1/winfsp-2.0.23055.msi %temp%\winfsp
 
-rem Uses msiexec to install the MSI file silently
-msiexec /i %temp%\winfsp /quiet /norestart ADDLOCAL=Core,Developer
+rem Uses msiexec to install the MSI file silently and install all developer options
+msiexec /i %temp%\winfsp /quiet /norestart ADDLOCAL=ALL
 
 rem Delete the MSI file
 del %temp%\winfsp
