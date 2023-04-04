5d8a00a1501e9a52d1833e4766e184485dfe46ed
James Matthew Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-04-04 15:48:53 -0600

    Fix flags

--- component/s3storage/client_test.go
+++ component/s3storage/client_test.go
@@ -1,5 +1,5 @@
-//go:build !authtest || !unittest
-// +build !authtest !unittest
+//go:build !authtest && !unittest
+// +build !authtest,!unittest
 
 /*
     _____           _____   _____   ____          ______  _____  ------
