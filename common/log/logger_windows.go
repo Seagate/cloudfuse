1b7bf250a5520dae8afd5bd68933b0e2525c6348
David Habinsky <david.habinsky@seagate.com>
2023-04-11 12:28:12 -0600

    remove second if that checks NoSyslogService

--- common/log/logger_windows.go
+++ common/log/logger_windows.go
@@ -20,9 +20,7 @@ func NewLogger(name string, config common.LogConfig) (Logger, error) {
 	if name == "syslog" {
 		sysLogger, err := newSysLogger(config.Level, config.Tag)
 		if err != nil {
-			if err == NoSyslogService {
-				return NewLogger("base", config)
-			}
+			return NewLogger("base", config)
 		}
 		return sysLogger, nil
 	} else if name == "silent" {
