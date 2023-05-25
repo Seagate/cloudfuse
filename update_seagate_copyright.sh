03066be8d99896a13e1eeee17352a44a9ac82b45
James Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-05-25 15:41:37 -0600

    Update shell script

--- update_seagate_copyright.sh
+++ update_seagate_copyright.sh
@@ -11,49 +11,40 @@ tmp_dir=$(mktemp -d)
 # Clone repo to temp location
 git clone "$blobfuse2_repo" "$tmp_dir" || { exit 1; }
 
-cd "$tmp_dir"
+cd "$tmp_dir" || { exit 1; }
 git checkout "$tags" || { exit 1; }
-cd -
+cd - || { exit 1; }
 
-# Get list of diff files
-diff_files=$(diff -x .git -qr . "$tmp_dir" | grep differ | awk '{print $2}')
+# Get list of files that we have modifed
+# This diff command prints output like as follows:
+#   Files ./cmd/mount.go and /tmp/tmp.yyhbYNR6UN/cmd/mount.go differ
+# Then get the lines that differ and get the path to the file that differs
+diff_files=$(diff -x .git -qr . "$tmp_dir" | grep "differ" | awk '{print $2}')
 
-# Get list of file that are new and do not exist in original repo
-new_files=$(diff -x .git -qr . "$tmp_dir" | grep "Only in ." | awk -F': ' '{print $2}')
+# Get list of file that we added and do not exist in original repo
+# This diff command prints output like as follows:
+#   Only in ./cmd: mount_linux_test.go
+#Then get the lines that are only new in our repo get the file name
+new_files=$(diff -x .git -qr . "$tmp_dir" | grep "Only in \." | awk '{print $3 "/" $4}' | sed 's/://')
 
 # Get a list of all diff files and new files
-all_file="$diff_files"
-
-# New files only have the file name, so we need to find the full path
-for file in $new_files
-do
-    found_file=$(find . -name "$file")
-
-    if [ -n "$found_file" ]
-    then
-        all_file+="$found_file"$'\n'
-    fi
-done
+all_file="$diff_files \n $new_files"
 
 for file in $all_file
 do
     # Check if the file exists in our repo
     if [ -f "$file" ]
     then
-        # If this file has a copyright statement
-        if grep -q "Copyright © .* Microsoft Corporation" "$file"
+        # Get the line number
+        line_number=$(grep -n "Copyright © .* Microsoft Corporation" "$file" | cut -d : -f 1 | head -n 1)
+        # Check for line number
+        if [ -n "$line_number" ]
         then
-            # Get the line number
-            line_number=$(grep -n "Copyright © .* Microsoft Corporation" "$file" | cut -d : -f 1 | head -n 1)
-            # Check for line number
-            if [ -n "$line_number" ]
+            # Check if the our copyright notice already exists
+            if ! grep -q "Copyright © .* Seagate Technology" "$file"
             then
-                # Check if the our copyright notice already exists
-                if ! grep -q "Copyright © .* Seagate Technology" "$file"
-                then
-                    # Add the new copyright notice above the one from Microsoft
-                    sed -i "${line_number}i \ \ \ Copyright © $year Seagate Technology LLC and/or its Affiliates" "$file"
-                fi
+                # Add the new copyright notice above the one from Microsoft
+                sed -i "${line_number}i \ \ \ Copyright © $year Seagate Technology LLC and/or its Affiliates" "$file"
             fi
         fi
     fi
