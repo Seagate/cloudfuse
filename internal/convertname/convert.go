6b42c6abd329a9f6fa1fb4d5d8b8062a67868e8c
James Matthew Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-06-13 11:23:39 -0600

    Add initial convert code

---
+++ internal/convertname/convert.go
@@ -0,0 +1,78 @@
+/*
+    _____           _____   _____   ____          ______  _____  ------
+   |     |  |      |     | |     | |     |     | |       |            |
+   |     |  |      |     | |     | |     |     | |       |            |
+   | --- |  |      |     | |-----| |---- |     | |-----| |-----  ------
+   |     |  |      |     | |     | |     |     |       | |       |
+   | ____|  |_____ | ____| | ____| |     |_____|  _____| |_____  |_____
+
+
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+
+   Copyright © 2023 Seagate Technology LLC and/or its Affiliates
+   Author : <blobfusedev@microsoft.com>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE
+*/
+
+package convertname
+
+var cloudToFileMap = map[rune]rune{
+	'/':  '／',
+	'"':  '＂',
+	'*':  '＊',
+	':':  '：',
+	'<':  '＜',
+	'>':  '＞',
+	'?':  '？',
+	'\\': '＼',
+	'|':  '｜',
+}
+
+var fileToCloudMap = reverseMap(cloudToFileMap)
+
+func reverseMap(inMap map[rune]rune) map[rune]rune {
+	var reverseMap = make(map[rune]rune)
+	for k, v := range inMap {
+		reverseMap[v] = k
+	}
+	return reverseMap
+}
+
+func FileToCloud(filename string) string {
+	runes := []rune(filename)
+	for i, v := range runes {
+		val := fileToCloudMap[v]
+		if val != 0 {
+			runes[i] = val
+		}
+	}
+	return string(runes)
+}
+
+func CloudToFile(cloudname string) string {
+	runes := []rune(cloudname)
+	for i, v := range runes {
+		val := cloudToFileMap[v]
+		if val != 0 {
+			runes[i] = val
+		}
+	}
+	return string(runes)
+}
---
+++ internal/convertname/convert_test.go
@@ -0,0 +1,68 @@
+/*
+    _____           _____   _____   ____          ______  _____  ------
+   |     |  |      |     | |     | |     |     | |       |            |
+   |     |  |      |     | |     | |     |     | |       |            |
+   | --- |  |      |     | |-----| |---- |     | |-----| |-----  ------
+   |     |  |      |     | |     | |     |     |       | |       |
+   | ____|  |_____ | ____| | ____| |     |_____|  _____| |_____  |_____
+
+
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+
+   Copyright © 2023 Seagate Technology LLC and/or its Affiliates
+   Author : <blobfusedev@microsoft.com>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE
+*/
+
+package convertname
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type conversionTestSuite struct {
+	suite.Suite
+	assert *assert.Assertions
+}
+
+func (suite *conversionTestSuite) SetupTest() {
+	suite.assert = assert.New(suite.T())
+}
+
+func (suite *conversionTestSuite) TestCloudToFile() {
+	filename := "test\"*:<>?\\|"
+	cloudname := "test＂＊：＜＞？＼｜"
+	result := CloudToFile(filename)
+	suite.assert.EqualValues(cloudname, result)
+}
+
+func (suite *conversionTestSuite) TestFileToCloud() {
+	filename := "test＂＊：＜＞？＼｜"
+	cloudname := "test\"*:<>?\\|"
+	result := FileToCloud(filename)
+	suite.assert.EqualValues(cloudname, result)
+}
+
+func TestConversionTestSuite(t *testing.T) {
+	suite.Run(t, new(conversionTestSuite))
+}
