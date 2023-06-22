36d459336ca234e9f2e251c13367e318c6093901
James Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-06-22 14:47:02 -0600

    Fix copyright statement

--- internal/convertname/convert_test.go
+++ internal/convertname/convert_test.go
@@ -10,7 +10,6 @@
    Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 
    Copyright © 2023 Seagate Technology LLC and/or its Affiliates
-   Author : <blobfusedev@microsoft.com>
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
