04c05005c5b6351ad864644f0e5014e3c3782931
James Fantin-Hardesty <james.fantin-hardesty@seagate.com>
2023-06-23 16:36:20 -0600

    Remove some uses of syscall and replace with corresponding package

--- common/util_linux.go
+++ common/util_linux.go
@@ -38,15 +38,16 @@ package common
 
 import (
 	"fmt"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // NotifyMountToParent : Send a signal to parent process about successful mount
 func NotifyMountToParent() error {
 	if !ForegroundMount {
-		ppid := syscall.Getppid()
+		ppid := unix.Getppid()
 		if ppid > 1 {
-			if err := syscall.Kill(ppid, syscall.SIGUSR2); err != nil {
+			if err := unix.Kill(ppid, unix.SIGUSR2); err != nil {
 				return err
 			}
 		} else {
