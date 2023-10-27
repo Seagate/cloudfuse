f471f06dcfc4ced79e61dd5ea5160b286d7d871f
Michael Habinsky <foodprocessor@users.noreply.github.com>
2023-10-27 15:04:15 -0600

    Set UserAgent / AppID for cloud storage requests. (#28)

--- component/azstorage/utils.go
+++ component/azstorage/utils.go
@@ -55,7 +55,7 @@ import (
 //    ----------- Helper to create pipeline options ---------------
 
 var UserAgent = func() string {
-	return "Azure-Storage-Fuse/" + common.CloudfuseVersion
+	return "Seagate-Cloudfuse/" + common.CloudfuseVersion
 }
 
 const (
