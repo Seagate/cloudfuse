342453d0b232627ecc7634342b3a09ae90511ea7
David Habinsky <david.habinsky@seagate.com>
2023-10-31 09:33:02 -0600

    Update component/attr_cache/attr_cache.go
    Co-authored-by: Michael Habinsky <foodprocessor@users.noreply.github.com>
    Signed-off-by: David Habinsky <davidhabinsky@gmail.com>

--- component/attr_cache/attr_cache.go
+++ component/attr_cache/attr_cache.go
@@ -795,7 +795,6 @@ func (ac *AttrCache) GetAttr(options internal.GetAttrOptions) (*internal.ObjAttr
 		// cache this entity not existing
 		// TODO: change the tests to no longer use empty structs. use internal.createAttr() to define a path instead of a literal.
 		ac.cacheMap.insert(&internal.ObjAttr{Path: internal.TruncateDirName(options.Name)}, false, time.Now())
-
 	}
 	return pathAttr, getErr
 }
