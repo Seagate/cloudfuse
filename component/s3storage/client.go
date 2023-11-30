686f12a77c3bd88eb69ddf0860e9fa3d4b5be9f1
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2023-11-30 09:28:15 -0700

    Fix issue with region with localstack

--- component/s3storage/client.go
+++ component/s3storage/client.go
@@ -127,7 +127,7 @@ func (cl *Client) Configure(cfg Config) error {
 		config.WithCredentialsProvider(credentialsProvider),
 		config.WithEndpointResolverWithOptions(endpointResolver),
 		config.WithAppID(UserAgent()),
-		config.WithRegion("auto"),
+		config.WithRegion(cl.Config.authConfig.Region),
 	)
 	if err != nil {
 		log.Err("Client::Configure : config.LoadDefaultConfig() failed. Here's why: %v", err)
