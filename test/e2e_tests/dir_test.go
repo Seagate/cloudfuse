4846860e8cd019dae1f1d37b9e5fd9b707e406e2
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2024-01-17 10:27:30 -0700

    Update copyright to 2024

--- test/e2e_tests/dir_test.go
+++ test/e2e_tests/dir_test.go
@@ -4,7 +4,7 @@
 /*
    Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 
-   Copyright © 2023 Seagate Technology LLC and/or its Affiliates
+   Copyright © 2023-2024 Seagate Technology LLC and/or its Affiliates
    Copyright © 2020-2023 Microsoft Corporation. All rights reserved.
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
