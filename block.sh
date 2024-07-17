47a4afe0f88635f7203dda1d61656558dbd5dacc
ahuynh1289 <andrew.huynh@seagate.com>
2024-07-17 09:27:21 -0600

    Cleanup log files

--- block.sh
+++ block.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#command to block outgoing calls to the cloud
 sudo iptables -A OUTPUT -d 192.55.6.0/24 -j REJECT
--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#command to accept outgoing calls to the cloud
 sudo iptables -D OUTPUT -d 192.55.6.0/24 -j REJECT
