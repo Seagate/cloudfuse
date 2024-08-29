505b022d7f0337b3bab9f75db160c9a0ea294c8d
Michael Habinsky <michael.habinsky@seagate.com>
2024-08-29 10:50:01 -0600

    Insert block rules at the beginning of the iptables OUTPUT chain.

--- test-scripts/block.sh
+++ test-scripts/block.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #command to block outgoing calls to the cloud
-sudo iptables -A OUTPUT -d 192.55.6.0/24 -j REJECT
-sudo iptables -A OUTPUT -d 20.60.153.33 -j REJECT
+sudo iptables -I OUTPUT 1 -d 192.55.6.0/24 -j REJECT
+sudo iptables -I OUTPUT 1 -d 20.60.153.33 -j REJECT
