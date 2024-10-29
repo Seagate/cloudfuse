cd8352616eb3a3387a6118aaaa25405f0d755394
Leata Byers <83234598+Ferelith-maker@users.noreply.github.com>
2024-10-29 08:48:29 -0600

    Change save button behavior in widgets (#343)
    * Add savebutton state, change exit behavior to check for save button
    
    * test pyside6 version
    
    * test pyside version for passing

--- gui/common_qt_functions.py
+++ gui/common_qt_functions.py
@@ -283,6 +283,7 @@ class widgetCustomFunctions(customConfigFunctions,QWidget):
         super().__init__()
 
     def exitWindow(self):
+        self.saveButtonClicked = True
         self.close()
 
     def exitWindowCleanup(self):
@@ -309,13 +310,8 @@ class widgetCustomFunctions(customConfigFunctions,QWidget):
         msg.setText("The settings have been modified.")
         msg.setStandardButtons(QtWidgets.QMessageBox.Discard | QtWidgets.QMessageBox.Cancel | QtWidgets.QMessageBox.Save)
         msg.setDefaultButton(QtWidgets.QMessageBox.Cancel)
-        ret = msg.exec()
-        if ret == QtWidgets.QMessageBox.Discard:
-            self.exitWindowCleanup()
-            event.accept()
-        elif ret == QtWidgets.QMessageBox.Cancel:
-            event.ignore()
-        elif ret == QtWidgets.QMessageBox.Save:
+
+        if self.saveButtonClicked == True:
             # Insert all settings to yaml file
             self.exitWindowCleanup()
             self.updateSettingsFromUIChoices()
@@ -323,6 +319,21 @@ class widgetCustomFunctions(customConfigFunctions,QWidget):
                 event.accept()
             else:
                 event.ignore()
+        else:
+            ret = msg.exec()
+            if ret == QtWidgets.QMessageBox.Discard:
+                self.exitWindowCleanup()
+                event.accept()
+            elif ret == QtWidgets.QMessageBox.Cancel:
+                event.ignore()
+            elif ret == QtWidgets.QMessageBox.Save:
+                # Insert all settings to yaml file
+                self.exitWindowCleanup()
+                self.updateSettingsFromUIChoices()
+                if self.writeConfigFile(self.settings):
+                    event.accept()
+                else:
+                    event.ignore()
 
     def updateSettingsFromUIChoices(self):
         # Each individual widget will need to override this function
