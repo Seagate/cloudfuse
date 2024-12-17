09113c4e473901714f36da3ae52179d66efb42fc
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2024-12-17 15:32:06 -0700

    Lint shell scripts and markdown files

--- test-scripts/e2e-test.sh
+++ test-scripts/e2e-test.sh
@@ -7,11 +7,11 @@ set -o nounset
 source ./helper/var.env
 
 # Mount the directory
-source ./helper/mount.sh
-if [ $? -ne 0 ]; then
-    echo "command failed with exit code $?"
+if ! source ./helper/mount.sh; then
+    ret=$?
+    echo "command failed with exit code $ret"
     echo "Stopping script"
-    exit $?
+    exit $ret
 fi
 
 # Run e2e tests
@@ -19,7 +19,7 @@ echo "-------------------------------------------------------------------"
 echo "Starting End-to-End Test"
 
 # run test
-go test -v -timeout=2h ../test/e2e_tests/data_validation_test.go ../test/e2e_tests/dir_test.go ../test/e2e_tests/file_test.go -args -mnt-path=$MOUNT_DIR -adls=false -clone=false -tmp-path=$MOUNT_TMP -quick-test=true -stream-direct-test=false -distro-name="Ubuntu"
+go test -v -timeout=2h ../test/e2e_tests/data_validation_test.go ../test/e2e_tests/dir_test.go ../test/e2e_tests/file_test.go -args -mnt-path="$MOUNT_DIR" -adls=false -clone=false -tmp-path="$MOUNT_TMP" -quick-test=true -stream-direct-test=false -distro-name="Ubuntu"
 
 # Cleanup test
 source ./helper/cleanup.sh
--- test-scripts/helper/cleanup.sh
+++ test-scripts/helper/cleanup.sh
@@ -8,12 +8,12 @@ source ./helper/var.env
 
 # cleanup step
 echo "Ensuring no container mounted in mount directory"
-sudo fusermount -u $MOUNT_DIR
-sudo fusermount3 -u $MOUNT_DIR
+sudo fusermount -u "$MOUNT_DIR"
+sudo fusermount3 -u "$MOUNT_DIR"
 
 echo "Stopping previous run of cloudfuse"
-sudo kill -9 `pidof cloudfuse` || true
+sudo kill -9 "$(pidof cloudfuse)" || true
 
 echo "Deleting files in mount and temp directories"
-rm -rf $MOUNT_DIR/*
-rm -rf $MOUNT_TMP/*
+rm -rf "${MOUNT_DIR:?}/*"
+rm -rf "${MOUNT_TMP:?}/*"
