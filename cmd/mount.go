8e4368babcc94b5b4caca093ed66ef494dc62067
David Habinsky <davidhabinsky@gmail.com>
2025-01-28 08:43:06 -0700

    corrected error checking

--- cmd/mount.go
+++ cmd/mount.go
@@ -355,13 +355,16 @@ var mountCmd = &cobra.Command{
 			// TODO: refer to the getBucketListS3() that mount_all does and getContainerListAzure() and let the restuls determine path.
 
 			containers, err := getBucketListS3()
-			if len(containers) != 0 && err != nil {
+			if len(containers) != 0 && err == nil {
 				pipeline = append(pipeline, "s3storage")
 			}
 
 			containers, err = getContainerListAzure()
-			if len(containers) != 0 && err != nil {
+			if len(containers) != 0 && err == nil {
 				pipeline = append(pipeline, "azstorage")
+			}
+			if err != nil {
+				return fmt.Errorf("failed to mount instance [%s]", err.Error())
 			}
 
 			options.Components = pipeline
