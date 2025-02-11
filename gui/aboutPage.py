800944a463951abf4adc7e48f66a0600f48c3f02
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2025-02-11 16:46:24 -0700

    Fix grammar issues

--- gui/aboutPage.py
+++ gui/aboutPage.py
@@ -26,16 +26,16 @@ from PySide6.QtCore import Qt
 class aboutPage(QMessageBox):
     def __init__(self, cloudfuseVersion: str):
         super().__init__()
-        self.setWindowTitle("About Cloudfuse")
+        self.setWindowTitle('About Cloudfuse')
         self.setTextFormat(Qt.RichText)
         self.setText(f"""
             <p><strong><h3>About Cloudfuse</h3></strong></p>
-            <p>The program using Cloudfuse version {cloudfuseVersion}</p>
+            <p>This program is using Cloudfuse version {cloudfuseVersion}</p>
             <p>Cloudfuse provides a virtual filesystem backed by either S3 or Azure Storage for mounting the cloud to a local system.</p>
             <p>For more information and frequently asked questions please go to the <a href="https://github.com/Seagate/cloudfuse">Cloudfuse GitHub</a>.</p>
             <p><h4>Help</h4></p>
-            <p>Found a bug? Report it at the <a href="https://github.com/Seagate/cloudfuse/issues">github issues</a> with the bug label.</p>
-            <p>Have questions not in the FAQ? Submit it at the <a href="https://github.com/Seagate/cloudfuse/issues">github issues</a> with the question label.</p>
+            <p>Found a bug? Report it at <a href="https://github.com/Seagate/cloudfuse/issues">github issues</a> with the bug label.</p>
+            <p>Have questions not in the FAQ? Submit it at <a href="https://github.com/Seagate/cloudfuse/issues">github issues</a> with the question label.</p>
             <p><h4>Third Party Notices</h4></p>
             <p>See <a href="NOTICE">notices</a> for third party license notices.</p>
             <p>WinSFP is licensed under the GPLv3 license with a special exception for Free/Libre and Open Source Software, which is available <a href="https://github.com/winfsp/winfsp/blob/master/License.txt">here</a>.</p>
