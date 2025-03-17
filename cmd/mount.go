874446f778f6398a1a883c642a0f2cf9640126b7
David Habinsky <davidhabinsky@gmail.com>
2025-03-17 10:35:17 -0600

    rearranged condition for consistency

--- cmd/mount.go
+++ cmd/mount.go
@@ -356,7 +356,7 @@ var mountCmd = &cobra.Command{
 
 			if containers, err := getBucketListS3(); len(containers) != 0 && err == nil {
 				pipeline = append(pipeline, "s3storage")
-			} else if containers, err = getContainerListAzure(); err == nil && len(containers) != 0 {
+			} else if containers, err = getContainerListAzure(); len(containers) != 0 && err == nil {
 				pipeline = append(pipeline, "azstorage")
 			} else if err != nil {
 				return fmt.Errorf("config does not specify any pipeline components. failed to determine correct cloud provider [%s]", err.Error())
