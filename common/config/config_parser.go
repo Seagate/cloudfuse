85e02003d41d2ed1bfc67352edf70e9836e70c1b
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2025-03-18 09:29:48 -0600

    Fix failing tests and update notice file

--- common/config/config_parser.go
+++ common/config/config_parser.go
@@ -106,6 +106,10 @@ func ReadFromConfigFile(configFilePath string) error {
 }
 
 func loadConfigFromBufferToViper(configData []byte) error {
+	// Set type to be yaml so that viper can parse the config data
+	// and since we only allow yaml formatted config files
+	viper.SetConfigType("yaml")
+
 	err := viper.ReadConfig(strings.NewReader(string(configData)))
 	if err != nil {
 		return err
