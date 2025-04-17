e23c6aca37b67c26f13708d25d8988ac1d50f541
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2025-04-17 08:47:50 -0600

    Fix linking for arm64 builds

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LDFLAGS="-s -w"
+
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ARCH=$(uname -m)
     if [[ "$ARCH" == "x86_64" ]]; then
@@ -14,35 +17,37 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     fi
 
     export CGO_ENABLED=1
+    export CGO_LDFLAGS="-L$LIBDIR"
     export CC="zig cc -target $ZIG_TARGET -isystem $LIBDIR -iwithsysroot /usr/include"
     export CXX="zig c++ -target $ZIG_TARGET -isystem $LIBDIR -iwithsysroot /usr/include"
+    LDFLAGS="$LDFLAGS -L $LIBDIR"
 
     if [[ "$1" == "fuse2" ]]; then
         rm -rf cloudfuse
         # Build cloudfuse with fuse2
-        go build -o cloudfuse
+        go build -o cloudfuse -ldflags="$LDFLAGS" -buildmode=pie -trimpath
     else
         rm -rf cloudfuse
         # Build cloudfuse with fuse3
-        go build -o cloudfuse -tags fuse3
+        go build -o cloudfuse -tags fuse3 -ldflags="$LDFLAGS" -buildmode=pie -trimpath
     fi
 
     # Build Health Monitor binary
     rm -rf cfusemon
-    go build -o cfusemon ./tools/health-monitor/
+    CGO_ENABLED=0 go build -o cfusemon -ldflags="$LDFLAGS" -buildmode=pie -trimpath ./tools/health-monitor/
 else
     rm -rf cfusemon
-    go build -o cfusemon.exe ./tools/health-monitor/
+    go build -o cfusemon.exe -ldflags="$LDFLAGS" -buildmode=pie -trimpath ./tools/health-monitor/
 
     # Build Windows Startup Tool
     rm -rf windows-startup
-    go build -o windows-startup.exe ./tools/windows-startup/
+    go build -o windows-startup.exe -ldflags="$LDFLAGS" -buildmode=pie -trimpath ./tools/windows-startup/
 
     # Build Windows Service Tool
     rm -rf windows-service
-    go build -o windows-service.exe ./tools/windows-service/
+    go build -o windows-service.exe -ldflags="$LDFLAGS" -buildmode=pie -trimpath ./tools/windows-service/
 
     # Build cloudfuse
     rm -rf cloudfuse.exe
-    go build -o cloudfuse.exe
+    go build -o cloudfuse.exe -ldflags="$LDFLAGS" -buildmode=pie -trimpath
 fi
