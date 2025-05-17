e7784c1011e169819268958a8b82ed70b523f486
Michael Habinsky <michael.habinsky@seagate.com>
2025-05-16 19:08:08 -0600

    Expand rule to include all Lyve Cloud regions (at least in the US).

--- test-scripts/block.sh
+++ test-scripts/block.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#command to block outgoing calls to the cloud
-sudo iptables -I OUTPUT 1 -d 192.55.6.0/24 -j REJECT
-sudo iptables -I OUTPUT 1 -d 20.60.153.33 -j REJECT
+#command to block outgoing calls to the Lyve Cloud
+sudo iptables -I OUTPUT 1 -d 192.55.0.0/16 -j REJECT
--- test-scripts/connect.sh
+++ test-scripts/connect.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#command to accept outgoing calls to the cloud
-sudo iptables -D OUTPUT -d 192.55.6.0/24 -j REJECT
-sudo iptables -D OUTPUT -d 20.60.153.33 -j REJECT
+#command to accept outgoing calls to Lyve Cloud
+sudo iptables -D OUTPUT -d 192.55.0.0/16 -j REJECT
