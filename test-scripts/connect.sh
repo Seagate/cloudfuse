226e5c71669647317e99cd0a753d30db84982849
Michael Habinsky <michael.habinsky@seagate.com>
2025-07-04 17:13:35 -0600

    Add Azure to offline test scripts

--- test-scripts/connect.sh
+++ test-scripts/connect.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#command to accept outgoing calls to Lyve Cloud
+#command to accept outgoing calls to Lyve Cloud and Azure Blob Storage
 sudo iptables -D OUTPUT -d 192.55.0.0/16 -j REJECT
+sudo iptables -D OUTPUT -d 20.60.0.0/16 -j REJECT
