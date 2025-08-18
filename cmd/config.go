d8d5f7096667d2bb61aebb0fd8ec24dbfbccbb23
brayan <brayan.a.trejo@seagate.com>
2025-08-18 16:01:54 -0600

    Add license to cmd/config.go

--- cmd/config.go
+++ cmd/config.go
@@ -1,3 +1,28 @@
+/*
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+
+   Copyright © 2023-2025 Seagate Technology LLC and/or its Affiliates
+   Copyright © 2020-2025 Microsoft Corporation. All rights reserved.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE
+*/
+
 package cmd
 
 import (
@@ -8,11 +33,11 @@ import (
 
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "Launch the interactive configuration TUI",
-	Long:  "Starts an interactive terminal-based UI to generate or edit your Cloudfuse configuration.",
+	Short: "Launch the interactive configuration tool.",
+	Long:  "Starts an interactive terminal-based UI to generate your Cloudfuse configuration file.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := runTUI(); err != nil {
-			return fmt.Errorf("failed to run TUI: %v", err)
+			return fmt.Errorf("Failed to run TUI: %v", err)
 		}
 		return nil
 	},
