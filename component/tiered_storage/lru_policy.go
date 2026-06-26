2ccb80fc17b91d8644140a98142f477ebc0ca644
Jacob Ho <jacob.ho@seagate.com>
2026-06-26 17:10:57 -0600

    Initial LRU Functions

--- component/tiered_storage/lru_policy.go
+++ component/tiered_storage/lru_policy.go
@@ -1,69 +1,96 @@
-
 package tiered_storage
 
 import (
-	"bytes"
-	"encoding/gob"
-	"os"
-	"path/filepath"
-	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 
-	"github.com/Seagate/cloudfuse/common"
 	"github.com/Seagate/cloudfuse/common/log"
 )
 
-type lruNode struct{
-	prev *lruNode  
+type lruNode struct {
+	prev *lruNode
 	next *lruNode
-	name string 
-	
+	name string
 }
+
 //upload 50 files and then check
 
-type lruQueue struct{
-	sync.Mutex
+type lruQueue struct {
+	mu sync.Mutex
 
-	wg sync.WaitGroup
-	numWorkers int 
+	nodeMap sync.Map
+
+	wg         sync.WaitGroup
+	numWorkers int
 
 	head *lruNode
-	tail *lruNode 
+	tail *lruNode
 
 	uploadChan chan string
-	doneChan chan struct{}
+	doneChan   chan struct{}
 
-	threshold float64
+	threshold   float64
 	targetRatio float64
-
-
 }
 
+func (q *lruQueue) Touch(name string) {}
 
-func(q *lruQueue) Touch(){}
+func (q *lruQueue) Enqueue(name string) {
+	//Maybe have a duplicate checker
 
-func(q *lruQueue) Add(){}
+	//create node
+	node := &lruNode{name: name}
+	//Add node to map
+	q.nodeMap.Store(name, node)
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	if q.head == nil {
+		q.head = node
+		q.tail = node
+	} else {
+		q.setHead(node)
+	}
+}
 
-func(q *lruQueue) Remove(){}
+func (q *lruQueue) Dequeue(name string) {
+	log.Trace("lruPolicy::removeNode : %s", name)
 
-//this will
+	var node *lruNode = nil
 
-func worker(){}
+	val, found := q.nodeMap.LoadAndDelete(name)
+	if !found || val == nil {
+		return
+	}
 
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	node = val.(*lruNode)
+
+	q.extractNode(node)
+}
+
+func (q *lruQueue) setHead(node *lruNode) {
+	// insert node at the head
+	node.prev = nil
+	node.next = q.head
+	q.head.prev = node
+	q.head = node
+}
+
+func worker() {}
 
 func (q *lruQueue) capacityChecker() {
-    defer q.wg.Done()
-    defer close(q.uploadChan) 
+	defer q.wg.Done()
+	defer close(q.uploadChan)
 
-    for {
-        select {
-        case <-time.After(//some time idk):
-            // eviction
+	for {
+		select {
+		case <-time.After(2 * time.Minute):
+			// eviction
 
-        case <-q.doneChan:
-            return
-        }
-    }
+		case <-q.doneChan:
+			return
+		}
+	}
 }
